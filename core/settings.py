fb46ee8349cc8d7387045a73a16a3743c6f3cd0b
elshangurbanow <elsenqurbanov50@gmail.com>
2025-04-25 21:26:37 +0000

    chanced

--- core/settings.py
+++ core/settings.py
@@ -31,9 +31,13 @@ INSTALLED_APPS = [
    # "django.contrib.staticfiles",
     "rest_framework",
     "api",
+    "corsheaders",
+
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',    
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -42,6 +46,12 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+
+CORS_ALLOWED_ORIGINS = [
+    "https://elshangurbanow.github.io",
+]
+
 
 ROOT_URLCONF = "core.urls"
 
